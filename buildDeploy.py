6eba59238803992ce09a95a798dfa55a64e27dd2
Dan Fitzpatrick <dan@Ocelots-MacBook-Pro.local>
2019-01-22 09:28:02 -0600

    take artifact name, location from CodePipeline, Notify CodePipeline of completion.

--- buildDeploy.py
+++ buildDeploy.py
@@ -5,15 +5,28 @@ import zipfile
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:842451556088:portfolioDeployment')
+    location = {
+        "bucketName": 'dwf-tmp',
+        "objectKey": 'portfolioBuild.zip'
+    }
 
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+                else:
+                    print "artifact name = " + artifact["name"]
+        print "Building portfolio from " + str(location)
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-        build_bucket = s3.Bucket('dwf-tmp')
+        build_bucket = s3.Bucket(location["bucketName"])
         target_bucket = s3.Bucket('dwf-portfolio')
 
         # On Windows, this will need to be a different location than /tmp
-        build_bucket.download_file('portfolioBuild.zip', '/tmp/portfolioBuild.zip')
+        build_bucket.download_file(location["objectKey"], '/tmp/portfolioBuild.zip')
 
         with zipfile.ZipFile('/tmp/portfolioBuild.zip') as myzip:
             for nm in myzip.namelist():
@@ -23,8 +36,13 @@ def lambda_handler(event, context):
 
         print 'Job Done!'
         topic.publish(Subject="Portfolio Deployment Status", Message="portfolio deployed successfully")
+
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+
     except:
         topic.publish(Subject="Portfolio Deployment FAILED", Message="portfolio deployment FAIILED!")
         raise
 
-    return "Hello from Lambda"
+    return "Complete"
