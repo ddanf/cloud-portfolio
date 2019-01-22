571a237d05d6f98c5b4cbe5b7fd1d399e98b35f3
Dan Fitzpatrick <dan@Ocelots-MacBook-Pro.local>
2019-01-21 23:12:40 -0600

    added SNS publishing to deply script

--- buildDeploy.py
+++ buildDeploy.py
@@ -3,6 +3,9 @@ from botocore.client import Config
 import zipfile
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:842451556088:portfolioDeployment')
+
     s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
     build_bucket = s3.Bucket('dwf-tmp')
@@ -17,4 +20,5 @@ def lambda_handler(event, context):
             target_bucket.upload_fileobj(obj, nm)
             target_bucket.Object(nm).Acl().put(ACL='public-read')
 
-    return 'Hello from Lambda'
+    print 'Job Done!'
+    topic.publish(Subject="Portfolio Deployment Status", Message="portfolio deployed successfully")
