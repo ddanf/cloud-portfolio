868f8a15957d0c223a0f218b305aeb112f7e5383
Dan Fitzpatrick <dan@Ocelots-MacBook-Pro.local>
2019-02-28 16:56:16 -0600

    added tests

---
+++ __tests__/example-work.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWork, { ExampleWorkBubble } from '../js/example-work';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+const myWork = [
+    {
+        'title': "Work Example",
+        'image': {
+            'desc': "example screenshot of a coding project",
+            'src': "images/example1.png",
+            'comment': ``
+        }
+    },
+    {
+        'title': "Dynamic Portfolio",
+        'image': {
+            'desc': "A serverless dynamic web portfolio",
+            'src': "images/example2.png",
+            'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
+                        https://www.flickr.com/photos/ssoosay/4097410999`
+        }
+    }
+];
+
+describe("ExampleWork component", () => {
+    let component = shallow(<ExampleWork work={myWork}/>)
+    it("Should be a section element", () => {
+        expect(component.type()).toEqual('section');
+    });
+
+    it("Should contain one child per data item", () => {
+        expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length);
+    });
+});
+
+describe("ExampleWorkBubble component", () => {
+    let component = shallow(<ExampleWorkBubble example={myWork[1]} />);
+    let images = component.find("img");
+
+    it("Should contain a single IMG element", () => {
+        expect(images.length).toEqual(1);
+    });
+
+    it("Should have the img src set correctly", () => {
+        expect(images.prop('src')).toEqual(myWork[1].image.src);
+    });
+});
